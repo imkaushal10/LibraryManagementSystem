4faeb5d4474b396f00c9e99a3fdefcb7785052a2
imkaushal10 <kb147181@gmail.com>
2020-06-10 21:24:07 +0545

    LMS

--- routes/bookrouter.js
+++ routes/bookrouter.js
@@ -35,104 +35,37 @@ bookrouter.route('/')
     });
 
 
-    //for task/:id
-//  taskrouter.route('/:id')
-//     .get((req,res, next) =>{
-//         Task.findById(req.params.id)
-//         .then((tasks)=>{
-//             res.json(tasks);
-//         }).catch(next);
-//     })
+    //for book/:id
+ taskrouter.route('/:id')
+    .get((req,res, next) =>{
+        Book.findById(req.params.id)
+        .then((bookss)=>{
+            res.json(books);
+        }).catch(next);
+    })
 
-//     .post((req, res)=>{
-//     res.status(401).send('Not Allowed');
-//     })
+    .post((req, res)=>{
+    res.status(401).send('Not Allowed');
+    })
 
-//     .put((req, res, next)=>{
-//         // res.send(`Will update task: ${req.params.id}`); //paramiterized string (`)=> (tille)
-//         Task.findById(req.params.id)
-//     .then((task)=>{
-//         if(req.body.name) task.name = req.body.name;
-//         if(req.body.done) task.done = req.body.done;
+    .put((req, res, next)=>{
+        // res.send(`Will update book: ${req.params.id}`); //paramiterized string (`)=> (tille)
+        Book.findById(req.params.id)
+    .then((book)=>{
+        if(req.body.name) book.name = req.body.name;
+          book.save()
+        .then((updatedbook)=>{
+            res.json(updatedbook);  
+        })  
+    }).catch(next);
+    })
 
-//         task.save()
-//         .then((updatedtask)=>{
-//             res.json(updatedtask);  
-//         })  
-//     }).catch(next);
-//     })
-
-//     .delete((req, res, next)=>{
-//         Task.findByIdAndRemove(req.params.id)
-//         .then((reply) =>{
-//             res.json(reply);
-//         }).catch(next);
-//     });
-
-// //for notes
-// taskrouter.route('/:taskid/notes')
-//     .get((req, res, next)=>{
-//         Task.findById(req.params.taskid)
-//         .then(task=>{
-//             res.json(task.notes);
-//         }).catch(err => next(err));
-//     })
-
-//     .post((req, res, next) =>{
-//         Task.findById(req.params.taskid)
-//         .then(task =>{
-//             task.notes.push(req.body);
-//             task.save()
-//             .then(updatedtask => {
-//                 res.status(201).json(updatedtask.notes);
-//             }).catch(next);
-//         }).catch(next);       
-//     })
-
-//     .delete((req, res, next) => {
-//         Task.findById(req.params.taskid)
-//         .then(task=>{
-//             task.notes = [];
-//             task.save()
-//             .then(updatedtask => {
-//                 res.json(updatedtask.notes);
-//             }).catch(next);
-//         }).catch(next);
-//     });
-
-// //for noteid
-// taskrouter.route('/:taskid/notes/:noteid')
-//     .get((req, res, next)=>{
-//         Task.findById(req.params.taskid)
-//         .then(task=>{
-//             res.json(task.notes.id(req.params.noteid));
-//         }).catch(next);
-//     })
-
-//     .put((req, res, next)=>{
-//         Task.findById(req.params.taskid)
-//         .then(task =>{
-//             let note = task.notes.id(req.params.noteid);
-//             note.text = req.body.text;
-//             task.save()
-//             .then(updatedtask=>{
-//                 res.json(task.notes.id(req.params.noteid));
-//             }).catch(next);
-//         }).catch(next);
-//     })
-
-//     .delete((req, res, next)=>{
-//         Task.findById(req.params.taskid)
-//         .then(task=>{
-//            task.notes = task.notes.filter((note) =>{
-//                return note.id !== req.params.noteid; 
-//            });
-//            task.save()
-//            .then(updatedtask=>{
-//                res.json(updatedtask.notes);
-//            }).catch(next);
-//         }).catch(next);
-//     });
+    .delete((req, res, next)=>{
+        Book.findByIdAndRemove(req.params.id)
+        .then((reply) =>{
+            res.json(reply);
+        }).catch(next);
+    });
 
 module.exports = bookrouter;
 
